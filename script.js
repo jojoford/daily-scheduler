5df8b4f2e0970d13e73faf061eca1300c27d3bba
jojoford <75343703+jojoford@users.noreply.github.com>
2021-01-24 17:44:27 -0600

    corrected pm display

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ var nineAm = $("#9am");
 var tenAm = $("#10am");
 var elevenAm = $("#11am");
 var twelvePm = $("#12pm");
-var onePm = $("#1pm");
-var twoPm = $("#2pm");
-var threePm = $("#3pm");
-var fourPm = $("#4pm");
-var fivePm = $("#5pm");
-var sixPm = $("#6pm");
-var sevenPm = $("#7pm");
-var eightPm = $("#8pm");
-var ninePm = $("#9pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+var eightPm = $("#20pm");
+var ninePm = $("#21pm");
 
 var hour = moment().hours();
 var userInput;
@@ -44,3 +44,62 @@ var interval = setInterval(function() {
     
     var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
     onePm.val(init1);
+    
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(init2);
+  
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(init3);
+ 
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(init4);
+  
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(init5);
+  
+  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+  sixPm.val(init6);
+  
+  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+  sevenPm.val(init7);
+} 
+
+
+function background () {
+      
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+  //      console.log(this);
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+  }
+
+  $(document).ready(function(){
+    initPage()
+    background()
+  
+    // Buttons (save to Local Storage)
+    $(".saveBtn").on("click", function(){
+      userInput = $(this).siblings(".form-control").val().trim();
+      console.log(userInput);
+      hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+      console.log(hourSpan);
+      localStorage.setItem(hourSpan, JSON.stringify(userInput));
+  
+    })
+    // Button for clear the day
+    $("#clearDay").on("click", function(){
+      localStorage.clear();
+      initPage()
+    }) 
+  
+  });
