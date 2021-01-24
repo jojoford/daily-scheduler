bff44ca9b2574a67735c628f5e3cbd6e50ed5d85
jojoford <75343703+jojoford@users.noreply.github.com>
2021-01-24 07:17:08 -0600

    js update

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // Moment.js
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
+// variables
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#1pm");
+var twoPm = $("#2pm");
+var threePm = $("#3pm");
+var fourPm = $("#4pm");
+var fivePm = $("#5pm");
+var sixPm = $("#6pm");
+var sevenPm = $("#7pm");
+var eightPm = $("#8pm");
+var ninePm = $("#9pm");
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+// var hourString = $(".hour").text().split(" ");
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
